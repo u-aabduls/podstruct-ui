fd6ad21b9ea87b32bd4b2eccef174e48feed6827
u-aabduls <umar@Umars-MacBook-Pro.local>
2022-06-08 18:31:45 -0400

    401 response to logout attempt now redirects to login page

--- src/connectors/Logout.js
+++ src/connectors/Logout.js
@@ -39,6 +39,7 @@ function __execute() {
         result.isSuccess = true;
         localStorage.clear();
     } else {
+        var data = JSON.parse(this.response)
         result.isSuccess = false;
         result.message = handleError(request.status, data)
     }
