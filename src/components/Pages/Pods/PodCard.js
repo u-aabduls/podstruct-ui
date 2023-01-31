b20666f9a4722d74f0bbb71d4268e6cc6985ebf0
u-aabduls <umar@Umars-MacBook-Pro.local>
2023-01-31 00:37:50 -0500

    feat(PMVP-198): update for release

--- src/components/Pages/Pods/PodCard.js
+++ src/components/Pages/Pods/PodCard.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { withRouter } from 'react-router';
-import { deactivatePod } from "../../../connectors/Pod";
-import { Card, CardHeader, CardBody, Table } from 'reactstrap';
+import { Card, CardHeader, Table } from 'reactstrap';
 
 class PodCard extends Component {
 
@@ -10,33 +9,8 @@ class PodCard extends Component {
         pod: ''
     }
 
-    deactivate = podId => {
-        var result = deactivatePod({ podId: podId });
-        if (result.isSuccess) {
-            window.location.href = window.location.href;
-        }
-    }
-
-    cardStyle = {
-        textDecoration: `none`
-    }
-
-    contentHeadingStyle = {
-        padding: `1rem`,
-        color: `white`,
-        backgroundColor: `#1797be`
-    }
-
-    contentBodyStyle = {
-        padding: `0rem`
-    }
-
-    tableDataStyle = {
-        paddingLeft: `1rem`
-    }
-
     componentDidMount() {
-        this.setState({pod: this.props.pod})
+        this.setState({ pod: this.props.pod })
     }
 
     componentDidUpdate(prevProps) {
@@ -68,41 +42,6 @@ class PodCard extends Component {
                     </Card >
                 </Link>
             </div>
-            /* <CardFooter className="text-center">
-                {this.state.action.length > 1 &&
-                    <div>
-                        <button
-                            className="btn btn-warning mr-2 mt-2 mb-2"
-                            title="Manage">
-                            Manage
-                        </button>
-                        <button
-                            className="btn btn-danger mt-2 mb-2"
-                            title="Deactivate"
-                            onClick={e => { this.deactivate(this.state.id) }}>
-                            Deactivate
-                        </button>
-                    </div>
-                }
-                {this.state.action.length === 1 && this.state.action[0] === "Manage" &&
-                    <div>
-                        <button
-                            className="btn btn-warning m-2"
-                            title="Manage">
-                            Manage
-                        </button>
-                    </div>
-                }
-                {this.state.action.length === 1 && this.state.action[0] === "View" &&
-                    <div>
-                        <button
-                            className="btn btn-primary m-2"
-                            title="View">
-                            View
-                        </button>
-                    </div>
-                }
-            </CardFooter> */
         )
     }
 }
